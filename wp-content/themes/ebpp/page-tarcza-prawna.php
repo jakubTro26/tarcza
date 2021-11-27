0b24eceb212d9bdb24732e067a902493d13a3713
kuba <info@weblead.pl>
2021-11-27 13:14:14 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -88,7 +88,7 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 	<div id="main_page_picture_div" style="height: 550px; background: #002f5d;">		
 		<div class="top_napisy">
-			<h3 style="text-align: center; font-size: 28px; color: #fff; text-shadow: 2px 2px #000;" class="show mobile">Opieka prawna na najwyższym poziomie</h3>
+			<h3 style="text-align: left;letter-spacing: 1px; font-size: 28px; color: #fff; text-shadow: 2px 2px #000;" class="show mobile">Opieka prawna na najwyższym poziomie</h3>
 			<div class="niezwlekaj">Nie zwlekaj i skorzystaj z oferty tarczy już teraz! Zamów kontakt a nasz doradca oddzwoni niezwłocznie.</div>
 			<div class="dostep">
 				<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p4.png">
