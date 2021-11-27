8fa3eb50a5f7bea668c8be0c152d53b51e36c1e1
kuba <info@weblead.pl>
2021-11-27 18:17:25 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -108,7 +108,10 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 					</div>
 				</div>
 			</div>
-		</div>		
+		</div>	
+		<div class="kontakt">
+
+		</div>	
 	</div>		
 
 	
