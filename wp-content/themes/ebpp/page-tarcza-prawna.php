2e329d8cb359d02d2dce26b1f3ed0be0c17e520d
kuba <info@weblead.pl>
2021-11-27 11:45:47 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -57,20 +57,18 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 <div id="main_div">
 	<div class="menu_info">
-		<div class="logo">
-			<a href="https://tarczadlaciebie.pl/"><img alt="Tarcza dla Ciebie - Jak Wyjść z Długów - pomoc w zadłużeniach" id="top_logo" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p1-1.png"></a>
-		</div>
-		<div class="koperta">
-			<a href="mailto:"><img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p2.png"></a>
-			<p>biuro@tarczadlaciebie.pl</p>
-
-		</div>
-
-		<div class="telefon">
-			<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p3.png">
-			<p>791092092</p>
-
-
+		<div class="topinfo">
+			<div class="logo">
+				<a href="https://tarczadlaciebie.pl/"><img alt="Tarcza dla Ciebie - Jak Wyjść z Długów - pomoc w zadłużeniach" id="top_logo" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p1-1.png"></a>
+			</div>
+			<div class="koperta">
+				<a href="mailto:"><img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p2.png"></a>
+				<p>biuro@tarczadlaciebie.pl</p>
+			</div>
+			<div class="telefon">
+				<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p3.png">
+				<p>791092092</p>
+			</div>
 		</div>
 	
 	</div>
