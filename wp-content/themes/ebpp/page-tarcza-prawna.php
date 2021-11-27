c5612eae7e6e8de16a279e7924b2db42aed3d7c9
kuba <info@weblead.pl>
2021-11-27 11:44:55 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -57,7 +57,9 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 <div id="main_div">
 	<div class="menu_info">
-		<div><a href="https://tarczadlaciebie.pl/"><img alt="Tarcza dla Ciebie - Jak Wyjść z Długów - pomoc w zadłużeniach" id="top_logo" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p1-1.png"></a></div>
+		<div class="logo">
+			<a href="https://tarczadlaciebie.pl/"><img alt="Tarcza dla Ciebie - Jak Wyjść z Długów - pomoc w zadłużeniach" id="top_logo" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p1-1.png"></a>
+		</div>
 		<div class="koperta">
 			<a href="mailto:"><img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p2.png"></a>
 			<p>biuro@tarczadlaciebie.pl</p>
