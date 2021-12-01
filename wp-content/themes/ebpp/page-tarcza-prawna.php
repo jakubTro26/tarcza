128aba8ab2a809ab4b122a7eada16438b18ec21d
kuba <info@weblead.pl>
2021-12-01 11:33:11 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -117,62 +117,63 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 		</div>	
 	</div>		
 	<div class="thirddiv">
-			<div class='specjalizujemy'>
-					Specjalizujemy się w
-			</div>
-			<div class="uscisk">
-				<img class="rece"  src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p10.png">
-			
-				<div class="orders">
-					<div class="column1">
-						<div class="u1">
-							<img class="i1" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class='n1'>
-								<span>Redukcji długów</span>
-							</div>
-						</div>
-						<div  class="u2">
-							<img class="i2" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class="n2" >
-								<span>Konsolidowaniu w jedną ratę</span>
-							</div>
-						</div>
-						<div  class="u3">
-							<img class="i3" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class="n3">
-								<span>Ochronie pensji i majątku</span>
-							</div>
-						</div>
-						<div  class="u4">
-							<img class="i4" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class="n4">
-								<span>Poprawie historii kredytowej</span>
-							</div>
-						</div>
-					</div>
-					<div class="column2">
-						<div  class="u5" >
-							<img class="i5" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class="n5">
-								<span>Upadłości konsumenckiej</span>
-							</div>
-						</div>
-						<div  class="u6" >
-							<img class="i6" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class="n6">
-								<span>Finansowaniu twoich potrzeb</span>
-							</div>
-						</div>
-						<div  class="u7">
-							<img class="i7" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
-							<div class="n7">
-								<span>Doradztwie antywindykacyjnym</span>
-							</div>
-						</div>
-					</div>
+			<div class="wrapper1">
+				<div class='specjalizujemy'>
+						Specjalizujemy się w
 				</div>
-			
-
+				<div class="uscisk">
+					<img class="rece"  src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p10.png">
+				
+					<div class="orders">
+						<div class="column1">
+							<div class="u1">
+								<img class="i1" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class='n1'>
+									<span>Redukcji długów</span>
+								</div>
+							</div>
+							<div  class="u2">
+								<img class="i2" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class="n2" >
+									<span>Konsolidowaniu w jedną ratę</span>
+								</div>
+							</div>
+							<div  class="u3">
+								<img class="i3" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class="n3">
+									<span>Ochronie pensji i majątku</span>
+								</div>
+							</div>
+							<div  class="u4">
+								<img class="i4" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class="n4">
+									<span>Poprawie historii kredytowej</span>
+								</div>
+							</div>
+						</div>
+						<div class="column2">
+							<div  class="u5" >
+								<img class="i5" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class="n5">
+									<span>Upadłości konsumenckiej</span>
+								</div>
+							</div>
+							<div  class="u6" >
+								<img class="i6" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class="n6">
+									<span>Finansowaniu twoich potrzeb</span>
+								</div>
+							</div>
+							<div  class="u7">
+								<img class="i7" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/11/p6.png">
+								<div class="n7">
+									<span>Doradztwie antywindykacyjnym</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+				</div>
 			</div>
 	</div>
 
