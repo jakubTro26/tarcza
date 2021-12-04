fdacc4986ca2ffaacdfe368a46656b3a5b7710cd
kuba <info@weblead.pl>
2021-12-04 08:54:30 +0100

    first

--- wp-content/themes/ebpp/index.php
+++ wp-content/themes/ebpp/index.php
@@ -328,9 +328,7 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 					</div>
 					<div class="p3">
-						<div class="belka3">
-							<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p24.png">
-						</div>
+						
 						<div class="img3">
 							<img class="number3" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p29.png">
 							<img class="photo3" src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p32.png">
