f02087d390b5a3a856ecb197fcbb780a79fce41e
kuba <info@weblead.pl>
2021-12-04 09:02:40 +0100

    first

--- wp-content/themes/ebpp/index.php
+++ wp-content/themes/ebpp/index.php
@@ -338,6 +338,11 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 						</div>
 					</div>
 				</div>
+				<div class="opis">
+						<div>
+							Nie zwlekaj, zamów bezpłatną konsultację i nawiąż kontakt z naszym doradcą
+						</div>
+				</div>
 			</div>
 	</div>
 
