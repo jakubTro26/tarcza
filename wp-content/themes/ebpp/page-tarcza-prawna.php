e9560ddd235cd8a65a5e87e7f0439a04cfc2207d
kuba <info@weblead.pl>
2021-12-04 14:13:17 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -106,7 +106,7 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 			</div>
 		</div>
 	</div>
-	<div class="thirddiv">
+	<div class="thirddiv2">
 			<div class="wrapper1">
 				<div class='specjalizujemy'>
 						Dobrze, że tutaj jesteś
