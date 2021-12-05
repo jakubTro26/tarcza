414d64476a85f7ddb8304504dc7af38550a008ff
kuba <info@weblead.pl>
2021-12-05 10:00:38 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -285,7 +285,10 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 				</div>
 
 			</div>
-			<div class="wrapperDlaczego">
+			
+	</div>
+
+	<div class="wrapperDlaczego">
 				<div class="dwrapper1">
 					<div class="dopis1">
 
@@ -373,9 +376,6 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 
 			</div>
-	</div>
-
-	
 
 
 
