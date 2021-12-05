2a989af898a11e3f0d2df8ec8db130adc4a4736d
kuba <info@weblead.pl>
2021-12-05 16:32:43 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-prawna.php
+++ wp-content/themes/ebpp/page-tarcza-prawna.php
@@ -637,28 +637,23 @@ window.location='https://tarczadlaciebie.pl/dziekujemy'
 	}
 
 	ToggleH1();
-</script><div id="side_contact_icon">
-	<a href="https://tarczadlaciebie.pl/kontakt/"><img src="<?php bloginfo('template_directory'); ?>/images/sluchawka.png" alt="zadzwoń do nas"></a>
-</div>
+</script>
+
+
 
 <script language="javascript">
 	//$("#side_contact_icon").appendTo("BODY");
 </script>
-<div id="zamow_kontakt_na_tle_div">
-<a name="form"></a>
 
-
-
-<hr style="width: 100%; background: #fff; color: #fff; box-shadow: 1px 2px 6px #000;">
 
 
 <?php
-if ( have_posts() ) :
-while ( have_posts() ) : the_post(229);
-?>
-<?php
-the_content();
-endwhile; endif;
+// if ( have_posts() ) :
+// while ( have_posts() ) : the_post(229);
+// ?>
+ <?php
+// the_content();
+// endwhile; endif;
 ?>
 
 
@@ -718,58 +713,19 @@ endwhile; endif;
 </script>
 
 
-<div id="top_bar_kontakt">
-	<div id="inner_top_bar_kontakt">
-		<a href="https://tarczadlaciebie.pl/kontakt"><h3 id="skontaktuj_top">SKONTAKTUJ SIĘ Z NAMI JUŻ TERAZ!</h3></a>
-
-		<div id="formularz_button">
-
-			<a href="https://tarczadlaciebie.pl/kontakt"><img src="<?php bloginfo('template_directory'); ?>/images/skonatkt-ads.png"></a>
-			<div id="top_bar_kontakt_telefon">791 092 092</div>
-			<a href="#zamow_kontakt_na_tle_div"><h3 class="tekst">FORMULARZ</h3></a>
-		</div>
-
-	</div>
-</div>
 
 
 
-<footer>
-<div id="stopka_div">
 
-		<div id="stopka_logo">
-			<img alt="Tarcza dla Ciebie - pomoc w zadłużeniach" src="<?php bloginfo('template_directory'); ?>/images/logo2.png" width="160" height="130">
-			
-		
-			<div id="stopka_logo_year">©2021</div>
-		</div>
-		<center>
-		<div id="stopka_menus">
-							
-							<div class="stopka_menu_group">
-								<div class="stopka_menu_group_title">Warunki</div>
-									<div class="stopka_menu_item">
-											<a href="https://tarczadlaciebie.pl/polityka-prywatnosci/">Polityka prywatności</a></div>
-										<div class="stopka_menu_item">
-											<a href="https://tarczadlaciebie.pl/regulamin/">Regulamin serwisu</a></div>
-										</div>
-																
-														
-							<div class="stopka_menu_group">
-							<div class="stopka_menu_group_title">Kontakt</div>
-							<div><div class="stopka_menu_item"><a href="https://tarczadlaciebie.pl/kontakt/">Telefon</a></div><div class="stopka_menu_item"><a href="https://tarczadlaciebie.pl/kontakt/">E-mail</a></div><div class="stopka_menu_item"><a href="#kontakt">Konsultant ONLINE</a></div></div></div>							<div class="stopka_menu_group">
-							<div class="stopka_menu_group_title">Społeczność</div>
-							<div><div class="stopka_menu_item"><a target="blank" href="https://facebook.com">Facebook</a></div><div class="stopka_menu_item"><a target="blank" href="https://google.com">Google+</a></div><div class="stopka_menu_item"><a target="blank" href="https://twitter.com/home">Twiter</a></div></div></div></div>
-		</center>
-	</div>
 
-</footer>
 
 	
 
 
 
 
-<?php wp_footer(); ?>
+<?php 
+// wp_footer();
+ ?>
 </body>
 </html>
