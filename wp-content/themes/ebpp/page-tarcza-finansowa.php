1d0dc3d51740ab2aa31ccb51e16b0d13613b87b5
kuba <info@weblead.pl>
2021-12-06 06:10:53 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-finansowa.php
+++ wp-content/themes/ebpp/page-tarcza-finansowa.php
@@ -142,15 +142,17 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 				
 					<div class="sawrapper">
 
-							<div class="imgsakwa">
-								<img class="sakwa"  src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/photo-tarcza-finansowa-dla-ciebie-1.png">
-							</div>
 							<div class="sakwanapis">
 								<div class="sanapis">
-									Szukasz kredytu ze słabą zdolnośćią? Wierzysz, że jak zasypiesz wszystkie banki zapytaniami, to któryś wkońcu zdecuduję się udzielić Ci nowego kredytu na spłatę zobowiązań? <br>Niestety to droga do nikąd, już po trzechm złożonych zapytaniach system bankowy zablokuje możliwosć udzielenia pożyczki.<br> Nie działąj na własną rękę i "na czuja".<br> My wiemy gdzie i kiedy. Wiemy który bank woli konsolidować kredyty, a który chwiliówki. Wiemy gdzie hotelarz a gdzie pielęgniarka najszybciej i najłatwiej otrzyma kredyt. Bez zapytań i bez strzelania na oślep.<br> Tak właśnie wyglądają finanse skrojone na miarę.
+									Wspieramy rozwój firm zapewniająć kapitał do rozwoju.<br> Jestemśmy jak świeży prąd dostarczony do maszyny, jak sportowe paliwo dla wyczynowego silnika. Sprawdzone finansowanie to szansa na wielki, wspólny krok naprzód.<br> Stawiamy na długofalową współpracę z Klientem i wspólny rozwój. Plan finansowy tworzony jest dla Ciebie po wnikliwej analizie potrzeb, z poszanowaniem kosztów i ekspresowo jak przelew BLIK. Jako miłośnicy idei pieniądza, działając w branży finansowej od lat wiemy, że szybkie decyzje biznesowe potrzebują szybkiego finansowania. Na potrzeby tworzonych planów finansowych doradzamy i podpowiadamy rozwiązania niezbędne do finansowania na rozwój przedsiębiorstwa.
 
 								</div>
 
+							</div>
+
+							
+							<div class="imgsakwa">
+								<img class="sakwa"  src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/photo-2-tarcza-finansowa.png">
 							</div>
 						
 						
