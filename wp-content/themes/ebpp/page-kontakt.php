7f8c32266ca3d3348804aac98bf4fbb0287f40bb
kuba <info@weblead.pl>
2021-12-07 12:33:06 +0100

    first

--- wp-content/themes/ebpp/page-kontakt.php
+++ wp-content/themes/ebpp/page-kontakt.php
@@ -147,7 +147,7 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 						</div>
 				</div>
-				<div class="ozdoba">
+				<div class="ozdoba ozdobakoperta">
 						<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p8.png">
 				</div>
 			<?php echo do_shortcode('[contact-form-7 id="97" title="Formularz 1"]'); ?>
