b4d184a6b38ce5d6ee41618edeb952688ac62466
kuba <info@weblead.pl>
2021-12-07 17:49:35 +0100

    first

--- wp-content/themes/ebpp/page-historia.php
+++ wp-content/themes/ebpp/page-historia.php
@@ -260,7 +260,7 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 
 		</div>
 		<div class="footer">
-		<div class="footerflex">
+		<div class="footerflex footerhistoria">
 				<div class="flex50">
 					<div class="tarczadlacb">
 									<div>
