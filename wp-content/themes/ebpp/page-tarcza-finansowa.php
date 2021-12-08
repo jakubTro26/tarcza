dc23e37d5d3865ed7a1b2ac52bf44964d78514d9
kuba <info@weblead.pl>
2021-12-08 09:51:54 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-finansowa.php
+++ wp-content/themes/ebpp/page-tarcza-finansowa.php
@@ -97,7 +97,7 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 				</div>
 				
 			</div>
-			<div class="kontakt2 kontakt ">
+			<div class="kontakt3 kontakt ">
 				<div class="ozdoba">
 						<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p8.png">
 				</div>
