482303ff553d952e92baa95d21bddb9a8d6151c5
kuba <info@weblead.pl>
2021-12-08 12:43:42 +0100

    first

--- wp-content/themes/ebpp/page-tarcza-finansowa.php
+++ wp-content/themes/ebpp/page-tarcza-finansowa.php
@@ -100,6 +100,14 @@ src="https://www.facebook.com/tr?id=1527878994231932&ev=PageView&noscript=1"
 			<div class="kontakt3 kontakt ">
 				<div class="ozdoba">
 						<img src="https://tarczadlaciebie.pl/wp-content/uploads/2021/12/p8.png">
+						<div class="opisformularz">
+							<div class="fooopis1">
+									FORMULARZ KONTAKTOWY
+							</div>
+							<div class="fooopis2">
+								*Pola obowiązkowe oznaczono gwiazdką
+							</div>
+						</div>
 				</div>
 			<?php echo do_shortcode('[contact-form-7 id="97" title="Formularz 1"]'); ?>
 			</div>
